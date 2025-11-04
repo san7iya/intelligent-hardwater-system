e80a231761ec71417396c6ed7ba3d63856c96a9e
Saniya Goyal <167917814+saniya7goyal@users.noreply.github.com>
2025-11-04 19:06:28 +0530

    added contextural recommendation to the nlp model and recommendation engine

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from models.arima_model import load_data, split_data, train_arima, evaluate_model
+from models.arima.arima_model import load_data, split_data, train_arima, evaluate_model
 from graphs.plot_forecast import plot_forecast, save_forecast
 
-from models.random_forest_model import load_water_data, train_random_forest
+from models.rf_model.random_forest_model import load_water_data, train_random_forest
 from graphs.plot_rf_results import plot_predictions, plot_feature_importance
 
-print("\n--- ARIMA MODEL RUN ---")
+from models.nlp_model.predict_nlp_v1 import predict_nlp_v2
+
+# Run ARIMA model
 series = load_data()
 train_data, test_data = split_data(series)
 model_fit = train_arima(train_data)
@@ -13,13 +15,20 @@ print(f"ARIMA RMSE: {rmse:.2f}")
 save_forecast(forecast, test_data.index)
 plot_forecast(train_data, test_data, forecast)
 
-print("\n--- RANDOM FOREST MODEL RUN ---")
+# Run Random Forest model
 df = load_water_data()
 model, features, (mae, rmse, r2), (y_test, y_pred) = train_random_forest(df)
-
 print(f"Mean Absolute Error: {mae:.2f}")
 print(f"Root Mean Squared Error: {rmse:.2f}")
 print(f"R² Score: {r2:.2f}")
-
 plot_predictions(y_test, y_pred)
 plot_feature_importance(model, features)
+
+# Run NLP model + Recommendation engine
+result = predict_nlp_v2()
+if result is not None:
+    estimated_value, prediction, _ = result
+    print(f"\nPredicted Category: {prediction}")
+    print(f"Estimated Hardness: {estimated_value} mg/L")
+else:
+    print("No prediction was made. Please enter a valid complaint description.")
